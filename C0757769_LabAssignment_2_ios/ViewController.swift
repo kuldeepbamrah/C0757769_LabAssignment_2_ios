218748be0d699a841573ae5f2a3dc31ee2add970
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-01-21 12:48:44 -0500

    Initial Commit

---
+++ C0757769_LabAssignment_2_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0757769_LabAssignment_2_ios
+//
+//  Created by MacStudent on 2020-01-21.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
